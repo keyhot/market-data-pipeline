7f5d25809eeaa09958cdbe4cc5d30ef3d3e0014c
keyhot <azamatov0202@gmail.com>
2026-01-20 14:55:37 +0100

    Chore: standardize file naming and ignore raw data

--- ingestion/storage.py
+++ ingestion/storage.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
 
+def utc_timestamp_str():
+    return pd.Timestamp.utcnow().strftime('%Y-%m-%dT%H-%M-%SZ')
+
 def store_ticker_csv(df, ticker_name: str, time_range: str):
-    timeframe_str = pd.Timestamp.now().strftime('%Y-%m-%d')
+    timeframe_str = utc_timestamp_str()
     file_path = f"data/raw/{ticker_name}_{time_range}_{timeframe_str}.csv"
     df.to_csv(file_path)
     return file_path
